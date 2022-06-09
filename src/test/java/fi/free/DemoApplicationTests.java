0ebbf2952253cff29bd44acbba672f7e69af0b74
ett15436 <ett15436@TREKT304.mshome.net>
2022-06-09 17:03:09 +0300

    Version 0.5.0
    - Added few unit test to secure SSN validation process

---
+++ src/test/java/fi/free/DemoApplicationTests.java
@@ -0,0 +1,43 @@
+package fi.free;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import fi.free.utils.SSNUtil;
+
+@SpringBootTest
+class DemoApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+	@DisplayName("Verifies that real SSN is accepted")
+	@Test
+	void testRealSocialSecurityNumberIsValid() {
+		assertTrue(SSNUtil.validateSSN("150285-537J"));
+		assertTrue(SSNUtil.validateSSN("040873-155V"));
+		assertTrue(SSNUtil.validateSSN("190501A6362"));
+		assertTrue(SSNUtil.validateSSN("200200+808K"));
+	}
+	
+	@DisplayName("Verifies that fake SSN is not accepted")
+	@Test
+	void testFakeSocialSecurityNumberIsNotValid() {
+		assertFalse(SSNUtil.validateSSN("123456-789A"));
+	}
+	
+	@DisplayName("Verifies that SSN validation is returning value even if invalid data format")
+	@Test
+	void testInvalidSocialSecurityNumberIsNotThrowingError() {
+		assertFalse(SSNUtil.validateSSN("ABR123456-789A"));
+		assertFalse(SSNUtil.validateSSN("356-789A"));
+		assertFalse(SSNUtil.validateSSN("ABR123456A789A"));
+		assertFalse(SSNUtil.validateSSN("!pr35Ay754--"));
+	}
+
+}
